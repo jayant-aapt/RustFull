0bd316f7f2092e1b7ef322d15113381ed8c2a0c9
Jayant <jayant.chand@aapttkenous.com>
2025-04-15 10:57:50 +0530

    Updated harcoded path  in main.rs

--- nats/src/main.rs
+++ nats/src/main.rs
@@ -1,20 +1,20 @@
 use std::process::Command;
 use std::path::Path;
-use nats::generate_nats_server_config;
+use shared_config::CONFIG;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // Path to the configuration file
-    let config_path = "d:/NewRustFull/RustFull/nats/nats-server.conf";
+    // Retrieve the configuration file path from CONFIG
+    let config_path = format!("{}/nats/nats-server.conf", CONFIG.app_dir);
 
     // Generate the configuration file dynamically (if needed)
-    if !Path::new(config_path).exists() {
-        generate_nats_server_config(config_path)?;
+    if !Path::new(&config_path).exists() {
+        nats::generate_nats_server_config(&config_path)?;
     }
 
     // Start the NATS server with the configuration file
     let mut child = Command::new("nats-server")
         .arg("-c")
-        .arg(config_path)
+        .arg(&config_path)
         .arg("-DV")
         .spawn()?;
 
